23f50f18cd82499592c85c526d49bb648a1e05f1
LgiMatBrd <contato@skorp.com.br>
2017-08-30 13:40:57 -0300

    teste 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 3000;
 
 app.get('/', function(req, res) {
-res.send('hello motherfuck! ZIKA1');
+res.send('hello motherfuck! ZIKA2');
 });
 
 app.listen(port, function(){
